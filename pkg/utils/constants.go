f519032f99b403c4099ab0b97b69026a20234a76
brunoisaac27 <brunoisaac2717@gmail.com>
2023-07-28 02:47:31 -0300

    :recycle: refactor: fights consts movidas para utils

--- pkg/utils/constants.go
+++ pkg/utils/constants.go
@@ -2,13 +2,7 @@ package models
 
 const (
 	
-	FightStyleNormal                 int = 4
-	FightStyleBoxing                 int = 5
-	FightStyleKungfu                 int = 6
-	FightStyleKneehead               int = 7
-	FightStyleGrabkick               int = 15
-	FightStyleElbow                  int = 16
-	
+
 
 	
 	
---
+++ pkg/utils/constants/fightsConst/fightsConst.go
@@ -0,0 +1,10 @@
+package fightsConsts
+
+const (
+	FightStyleNormal   int = 4
+	FightStyleBoxing   int = 5
+	FightStyleKungfu   int = 6
+	FightStyleKneehead int = 7
+	FightStyleGrabkick int = 15
+	FightStyleElbow    int = 16
+)
