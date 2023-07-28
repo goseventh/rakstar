e7ddc58fb0852bf45d14791a0c0efec264817909
brunoisaac27 <brunoisaac2717@gmail.com>
2023-07-28 03:37:52 -0300

    :recycle: refactor: 3dtext consts movidos para utils

--- pkg/utils/constants.go
+++ pkg/utils/constants.go
@@ -9,8 +9,7 @@ const (
 
 	
 
-	Max3dtextGlobal int = 1024
-	Invalid3dtextId int = 0xFFFF
+
 
 	MaxPickups int = 4096
 
---
+++ pkg/utils/constants/3dTextConst/3dTextConst.go
@@ -0,0 +1,5 @@
+package 3dTextConst
+const (
+	Max3dtextGlobal int = 1024
+	Invalid3dtextId int = 0xFFFF
+)
