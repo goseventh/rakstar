9bb0c16bd28c0efad6ed90f8af095b0ddb580334
brunoisaac27 <brunoisaac2717@gmail.com>
2023-07-28 02:50:19 -0300

    :recycle: refactor: vehicles const movidos para utils

---
+++ pkg/utils/constants/vehiclesConst/vehiclesConst.go
@@ -0,0 +1,21 @@
+package vehiclesConst
+
+const (
+	VehicleParamsUnset           int = -1
+	VehicleParamsOff             int = 0
+	VehicleParamsOn              int = 1
+	VehicleModelInfoSize         int = 1
+	VehicleModelInfoFrontseat    int = 2
+	VehicleModelInfoRearseat     int = 3
+	VehicleModelInfoPetrolcap    int = 4
+	VehicleModelInfoWheelsfront  int = 5
+	VehicleModelInfoWheelsrear   int = 6
+	VehicleModelInfoWheelsmid    int = 7
+	VehicleModelInfoFrontBumperZ int = 8
+	VehicleModelInfoRearBumperZ  int = 9
+	MaxClientMessage             int = 144
+	MaxVehicles                  int = 2000
+	MaxActors                    int = 1000
+	InvalidVehicleId             int = 0xFFFF
+	BulletHitTypeVehicle         int = 2
+)
