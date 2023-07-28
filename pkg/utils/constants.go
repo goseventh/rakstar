63aaf52e7edf127a19b67ded0a1f94e8f0ad9095
brunoisaac27 <brunoisaac2717@gmail.com>
2023-07-28 02:52:44 -0300

    :recycle: refactor: objects consts movidos para utils

--- pkg/utils/constants.go
+++ pkg/utils/constants.go
@@ -32,8 +32,10 @@ const (
 
 	InvalidActorId                   int = 0xFFFF
 	NoTeam                           int = 255
-	MaxObjects                       int = 1000
-	InvalidObjectId                  int = 0xFFFF
+
+
+	
+	
 	MaxGangZones                     int = 1024
 	MaxTextDraws                     int = 2048
 	MaxMenus                         int = 128
@@ -96,31 +98,13 @@ const (
 	EditResponseCancel               int = 0
 	EditResponseFinal                int = 1
 	EditResponseUpdate               int = 2
-	SelectObjectGlobalObject         int = 1
-	SelectObjectPlayerObject         int = 2
+
 	BulletHitTypeNone                int = 0
 	BulletHitTypePlayer              int = 1
 
-	BulletHitTypeObject              int = 3
-	BulletHitTypePlayerObject        int = 4
+	
 	DownloadRequestEmpty             int = 0
 	DownloadRequestModelFile         int = 1
 	DownloadRequestTextureFile       int = 2
-	ObjectMaterialSize32x32          int = 10
-	ObjectMaterialSize64x32          int = 20
-	ObjectMaterialSize64x64          int = 30
-	ObjectMaterialSize128x32         int = 40
-	ObjectMaterialSize128x64         int = 50
-	ObjectMaterialSize128x128        int = 60
-	ObjectMaterialSize256x32         int = 70
-	ObjectMaterialSize256x64         int = 80
-	ObjectMaterialSize256x128        int = 90
-	ObjectMaterialSize256x256        int = 100
-	ObjectMaterialSize512x64         int = 110
-	ObjectMaterialSize512x128        int = 120
-	ObjectMaterialSize512x256        int = 130
-	ObjectMaterialSize512x512        int = 140
-	ObjectMaterialTextAlignLeft      int = 0
-	ObjectMaterialTextAlignCenter    int = 1
-	ObjectMaterialTextAlignRight     int = 2
+
 )
---
+++ pkg/utils/constants/objectsConsts/objectsConst.go
@@ -0,0 +1,26 @@
+package objectsConsts
+const (
+	MaxObjects                       int = 1000
+	InvalidObjectId                  int = 0xFFFF
+	SelectObjectGlobalObject         int = 1
+	SelectObjectPlayerObject         int = 2
+	ObjectMaterialSize32x32          int = 10
+	ObjectMaterialSize64x32          int = 20
+	ObjectMaterialSize64x64          int = 30
+	ObjectMaterialSize128x32         int = 40
+	ObjectMaterialSize128x64         int = 50
+	ObjectMaterialSize128x128        int = 60
+	ObjectMaterialSize256x32         int = 70
+	ObjectMaterialSize256x64         int = 80
+	ObjectMaterialSize256x128        int = 90
+	ObjectMaterialSize256x256        int = 100
+	ObjectMaterialSize512x64         int = 110
+	ObjectMaterialSize512x128        int = 120
+	ObjectMaterialSize512x256        int = 130
+	ObjectMaterialSize512x512        int = 140
+	ObjectMaterialTextAlignLeft      int = 0
+	ObjectMaterialTextAlignCenter    int = 1
+	ObjectMaterialTextAlignRight     int = 2
+	BulletHitTypeObject              int = 3
+	BulletHitTypePlayerObject        int = 4
+)
