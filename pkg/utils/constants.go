c70cfd294b9a41a075290a254742d1f38f6fea4c
brunoisaac27 <brunoisaac2717@gmail.com>
2023-07-28 03:31:30 -0300

    :recycle: refactor: gangzone consts movidos para utils

--- pkg/utils/constants.go
+++ pkg/utils/constants.go
@@ -1,34 +1,27 @@
 package models
 
 const (
-	MaxChatbubbleLength     int = 144
+	MaxChatbubbleLength int = 144
 
 	InvalidActorId int = 0xFFFF
 
-	NoTeam         int = 255
-
-	MaxGangZones              int = 1024
-	InvalidGangZone           int = -1
-	
-	
-	Max3dtextGlobal           int = 1024
-	Invalid3dtextId           int = 0xFFFF
-
-	MaxPickups                int = 4096
+	NoTeam int = 255
 
 	
 
+	Max3dtextGlobal int = 1024
+	Invalid3dtextId int = 0xFFFF
+
+	MaxPickups int = 4096
 
 	//Weapon
 
-
 	ClickSourceScoreboard int = 0
 
-	EditResponseCancel    int = 0
-	EditResponseFinal     int = 1
-	EditResponseUpdate    int = 2
+	EditResponseCancel int = 0
+	EditResponseFinal  int = 1
+	EditResponseUpdate int = 2
 
 	BulletHitTypeNone   int = 0
 	BulletHitTypePlayer int = 1
-
 )
