ba3aa159d9b8b647b6e4c7e13a998ad447f0527d
brunoisaac27 <brunoisaac2717@gmail.com>
2023-07-28 02:54:12 -0300

    :recycle: refactor: keys consts movidos para utils

--- pkg/utils/constants.go
+++ pkg/utils/constants.go
@@ -64,29 +64,7 @@ const (
 
 	
 
-	KeyAction                        int = 1
-	KeyCrouch                        int = 2
-	KeyFire                          int = 4
-	KeySprint                        int = 8
-	KeySecondaryAttack               int = 16
-	KeyJump                          int = 32
-	KeyLookRight                     int = 64
-	KeyHandbrake                     int = 128
-	KeyLookLeft                      int = 256
-	KeySubmission                    int = 512
-	KeyLookBehind                    int = 512
-	KeyWalk                          int = 1024
-	KeyAnalogUp                      int = 2048
-	KeyAnalogDown                    int = 4096
-	KeyAnalogLeft                    int = 8192
-	KeyAnalogRight                   int = 16384
-	KeyYes                           int = 65536
-	KeyNo                            int = 131072
-	KeyCtrlBack                      int = 262144
-	KeyUp                            int = -128
-	KeyDown                          int = 128
-	KeyLeft                          int = -128
-	KeyRight                         int = 128
+	
 	BodyPartTorso                    int = 3
 	BodyPartGroin                    int = 4
 	BodyPartLeftArm                  int = 5
