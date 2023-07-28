efa3c081c4818ac718cdff4bf137252bc31393bb
brunoisaac27 <brunoisaac2717@gmail.com>
2023-07-28 02:46:29 -0300

    :recycle: refactor: special consts movida para utils

--- pkg/utils/constants.go
+++ pkg/utils/constants.go
@@ -1,26 +1,7 @@
 package models
 
 const (
-	SpecialActionNone                int = 0
-	SpecialActionDuck                int = 1
-	SpecialActionUsejetpack          int = 2
-	SpecialActionEnterVehicle        int = 3
-	SpecialActionExitVehicle         int = 4
-	SpecialActionDance1              int = 5
-	SpecialActionDance2              int = 6
-	SpecialActionDance3              int = 7
-	SpecialActionDance4              int = 8
-	SpecialActionHandsup             int = 10
-	SpecialActionUsecellphone        int = 11
-	SpecialActionSitting             int = 12
-	SpecialActionStopusecellphone    int = 13
-	SpecialActionDrinkBeer           int = 20
-	SpecialActionSmokeCiggy          int = 21
-	SpecialActionDrinkWine           int = 22
-	SpecialActionDrinkSprunk         int = 23
-	SpecialActionCuffed              int = 24
-	SpecialActionCarry               int = 25
-	SpecialActionPissing             int = 68
+	
 	FightStyleNormal                 int = 4
 	FightStyleBoxing                 int = 5
 	FightStyleKungfu                 int = 6
---
+++ pkg/utils/constants/specialConst/specialConst.go
@@ -0,0 +1,24 @@
+package specialConst
+
+const (
+	SpecialActionNone             int = 0
+	SpecialActionDuck             int = 1
+	SpecialActionUsejetpack       int = 2
+	SpecialActionEnterVehicle     int = 3
+	SpecialActionExitVehicle      int = 4
+	SpecialActionDance1           int = 5
+	SpecialActionDance2           int = 6
+	SpecialActionDance3           int = 7
+	SpecialActionDance4           int = 8
+	SpecialActionHandsup          int = 10
+	SpecialActionUsecellphone     int = 11
+	SpecialActionSitting          int = 12
+	SpecialActionStopusecellphone int = 13
+	SpecialActionDrinkBeer        int = 20
+	SpecialActionSmokeCiggy       int = 21
+	SpecialActionDrinkWine        int = 22
+	SpecialActionDrinkSprunk      int = 23
+	SpecialActionCuffed           int = 24
+	SpecialActionCarry            int = 25
+	SpecialActionPissing          int = 68
+)
