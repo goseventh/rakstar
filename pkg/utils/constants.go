24aab4b362923ca61553a9a2ef732faa944690e5
brunoisaac27 <brunoisaac2717@gmail.com>
2023-07-28 03:39:44 -0300

    :recycle: refactor: outherconsts criados/movidos para utils

--- pkg/utils/constants.go
+++ pkg/utils/constants.go
@@ -1,23 +1,5 @@
 package models
 
 const (
-	MaxChatbubbleLength int = 144
-
-	InvalidActorId int = 0xFFFF
-
-	NoTeam int = 255
-
-	
-
-
-
-	MaxPickups int = 4096
-
-	//Weapon
-
-	ClickSourceScoreboard int = 0
-
-
-
 	
 )
---
+++ pkg/utils/constants/outhersConst/outherConst.go
@@ -0,0 +1,9 @@
+package outherConst
+
+const (
+	MaxChatbubbleLength   int = 144
+	InvalidActorId        int = 0xFFFF
+	NoTeam                int = 255
+	MaxPickups            int = 4096
+	ClickSourceScoreboard int = 0
+)
