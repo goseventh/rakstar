0ad067eb2541f077b6d9e43df5ae9f164f38b7bb
brunoisaac27 <brunoisaac2717@gmail.com>
2023-08-13 15:51:43 -0300

    :sparkles: feat: adiciona prefixo e sufixo
    este commit adiciona os verificadores condicionais para prefixo e sufixo:
      MustHavePrefix - o texto deve ter o prefixo X
      MustHaveSufix  - o texto deve ter o sufixo  X

--- command/typeText.go
+++ command/typeText.go
@@ -3,6 +3,8 @@ package command
 const (
 	MustBeUppercase = iota
 	MustBeLowercase
+  MustHavePrefix
+  MustHaveSufix
   MustCompileRegex
 )
 
@@ -27,6 +29,15 @@ func (t *TypeText) MustBeLowercase() *TypeText {
 	return t
 }
 
+func (t *TypeText) MustHavePrefix() *TypeText {
+	t.c.createConditional(MustHavePrefix, t.c.index, nil)
+	return t
+}
+
+func (t *TypeText) MustHaveSufix() *TypeText {
+	t.c.createConditional(MustHaveSufix, t.c.index, nil)
+	return t
+}
 
 func (t *TypeText) MustCompileRegex(regex string) *TypeText {
 	t.c.createConditional(MustBeLowercase, t.c.index, regex)
