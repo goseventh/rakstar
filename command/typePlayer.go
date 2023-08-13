e83cb574b6ebca2c6436df6bcb3982102e5afc06
brunoisaac27 <brunoisaac2717@gmail.com>
2023-08-13 01:36:49 -0300

    :sparkles: feat: typeNumber
    este commit adiciona o typeNumber, com seus verificadores condicionais:
        MustDivisibleBy    - numero deve ser divisivel por X
        MustBeGreaterThan  - numero deve ser maior que X
        MustBeLessThan     - numero deve ser menor que X
        MustBeMultipleOf   - numero deve ser divisível por X
        MustBeBetween      - numero deve ser maior que X e menor que Y
        MustBeSquareRootOf - numero deve ser o resultado da raiz quadrada de X

--- command/typePlayer.go
+++ command/typePlayer.go
@@ -13,15 +13,12 @@ func (c *conditionalsBuilder) TypePlayer() *TypePlayer {
 	c.typeIdx = typePlayer
 	tPlayer := new(TypePlayer)
 	tPlayer.c = c
-
 	return tPlayer
 }
 
 func (t *TypePlayer) MustConnected() *TypePlayer {
  t.c.createConditional(MustPlayerConnected, t.c.index, nil)
-
 	return t
-
 }
 
 func (t *TypePlayer) MustNickIs(nick string) *TypePlayer {
