7b777fa02455bc6edb0e2e24553d8a4184378c48
brunoisaac27 <brunoisaac2717@gmail.com>
2023-08-13 01:36:49 -0300

    :sparkles: feat: typeNumber
    este commit adiciona o typeNumber, com seus verificadores condicionais:
        MustDivisibleBy    - numero deve ser divisivel por X
        MustBeGreaterThan  - numero deve ser maior que X
        MustBeLessThan     - numero deve ser menor que X
        MustBeMultipleOf   - numero deve ser divisível por X
        MustBeBetween      - numero deve ser maior que X e menor que Y
        MustBeSquareRootOf - numero deve ser o resultado da raiz quadrada de X

---
+++ command/typeNumber.go
@@ -0,0 +1,60 @@
+package command
+
+const (
+	MustBeDivisibleBy = iota
+	MustBeGreaterThan
+	MustBeLessThan
+	MustBeBetween
+  MustBeMultipleOf
+  MustBeSquareRootOf
+)
+
+type TypeNumber struct {
+	c *conditionalsBuilder
+}
+
+func (c *conditionalsBuilder) TypeNumber() *TypeNumber {
+	c.typeIdx = typeNumber
+	tNumber := new(TypeNumber)
+	tNumber.c = c
+	return tNumber
+}
+
+func (t *TypeNumber) MustBeDivisibleBy(x int) *TypeNumber {
+	t.c.createConditional(MustBeDivisibleBy, t.c.index, x)
+	return t
+}
+
+func (t *TypeNumber) MustBeGreaterThan(x int) *TypeNumber {
+	t.c.createConditional(MustBeGreaterThan, t.c.index, x)
+	return t
+}
+
+func (t *TypeNumber) MustBeLessThan(x int) *TypeNumber {
+	t.c.createConditional(MustBeLessThan, t.c.index, x)
+	return t
+}
+
+
+func (t *TypeNumber) MustBeMultipleOf(x int) *TypeNumber {
+	t.c.createConditional(MustBeMultipleOf, t.c.index, x)
+	return t
+}
+
+
+func (t *TypeNumber) MustBeSquareRootOf(x int) *TypeNumber {
+	t.c.createConditional(MustBeSquareRootOf, t.c.index, x)
+	return t
+}
+
+func (t *TypeNumber) MustBeBetween(x, y int) *TypeNumber {
+	var values []int
+	values = (append(values, x, y))
+	t.c.createConditional(MustBeBetween, t.c.index, values)
+	return t
+}
+
+func (t *TypeNumber) End() *conditionalsBuilder {
+	t.c.Set()
+	return t.c
+}
