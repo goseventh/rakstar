3cd69032a87941a391dc47192f53a748c3b7da47
brunoisaac27 <brunoisaac2717@gmail.com>
2023-08-18 20:29:53 -0300

    :books: docs: Modulo player
    este commit adiciona a documentação para os métodos do modulo player adiciona-
    dos no commit: 28e6bd36bb675cf727c7209e0b99be2a551eb365

--- player/player.go
+++ player/player.go
@@ -11,26 +11,31 @@ var (
 	FailTeleport = errors.New("Player teleportation failure")
 )
 
+// Seta a vida do player
 func (pb *PlayerBuilder) Life(life float32) *PlayerBuilder {
 	natives.SetPlayerHealth(pb.ID, life)
 	return pb
 }
 
+// Seta a armadura(colete) do jogador
 func (pb *PlayerBuilder) Armour(Armour float32) *PlayerBuilder {
 	natives.SetPlayerArmour(pb.ID, Armour)
 	return pb
 }
 
+// (re)Spawna o jogador
 func (pb *PlayerBuilder) Spawn() *PlayerBuilder {
 	natives.SpawnPlayer(pb.ID)
 	return pb
 }
 
+// Obtem o nick do jogador, e seta na variavel recebida no parametro
 func (pb *PlayerBuilder) Nick(nick *string) *PlayerBuilder {
 	natives.GetPlayerName(pb.ID, nick, playerConst.MaxPlayerName)
 	return pb
 }
 
+// Obtem o angulo do jogador
 func (pb *PlayerBuilder) GetAngle() float32 {
 	var angle float32
 	connected := natives.GetPlayerFacingAngle(pb.ID, &angle)
@@ -40,6 +45,7 @@ func (pb *PlayerBuilder) GetAngle() float32 {
 	return angle
 }
 
+// Obtem a posição do jogador
 func (pb *PlayerBuilder) GetPos() (float32, float32, float32) {
 	var (
 		x, y, z float32
@@ -51,6 +57,11 @@ func (pb *PlayerBuilder) GetPos() (float32, float32, float32) {
 	return x, y, z
 }
 
+// Invocar esta função teletransportará o jogador para as
+// coordenadas informadas no parametro, bem como sua direção 
+// de orientação - direção que corresponde, por exemplo, a 
+// rotação baseada na bússula. Ou seja, a direção que o jogador
+// está olhando 
 func (pb *PlayerBuilder) Teleport(x, y, z, r float32) error {
 	sucess := natives.SetPlayerPos(pb.ID, x, y, z)
 	sucess2 := natives.SetPlayerFacingAngle(pb.ID, r)
@@ -60,14 +71,18 @@ func (pb *PlayerBuilder) Teleport(x, y, z, r float32) error {
 	return nil
 }
 
+// Invocar esta função retornará se o jogador está dentro de um
+// veículo
 func (pb *PlayerBuilder) InVehicle() bool {
 	return natives.IsPlayerInAnyVehicle(pb.ID)
 }
 
+// Invocar esta função retornará o ID do veículo que o jogador está
 func (pb *PlayerBuilder) GetVehicle() int {
 	return natives.GetPlayerVehicleID(pb.ID)
 }
 
+// Invocar esta função destruirá o veículo do jogador
 func (pb *PlayerBuilder) DeleteCurrentVehicle() bool {
 	vehID := natives.GetPlayerVehicleID(pb.ID)
 	return natives.DestroyVehicle(vehID)
