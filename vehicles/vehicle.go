36fd32dbbdcd00db3338444607e6a15cfe06087e
brunoisaac27 <brunoisaac2717@gmail.com>
2023-08-18 19:50:12 -0300

    :sparkles: feat: DestroyInRanges
    este commit adiciona o recurso que destroi os veiculos próximos ao jogador

--- vehicles/vehicle.go
+++ vehicles/vehicle.go
@@ -1,7 +1,10 @@
 package vehicles
 
 import (
+	"math"
+
 	"github.com/goseventh/rakstar/internal/natives"
+	"github.com/goseventh/rakstar/internal/utils/constants/vehiclesConst"
 	"github.com/goseventh/rakstar/player"
 )
 
@@ -51,31 +54,60 @@ func (v *vehicleBuilder) Create() *vehicleBuilder {
 // Adiciona um jogador a um assento disponível de um veículo
 //
 // a função tentará encontrar quais assentos estão disponíveis
-// para o veiculo, caso houver setará automaticamente o jogador 
-func (v *vehicleBuilder) AttachPlayer(p *player.PlayerBuilder) *vehicleBuilder{
-	var seats[]int
+// para o veiculo, caso houver setará automaticamente o jogador
+func (v *vehicleBuilder) AttachPlayer(p *player.PlayerBuilder) *vehicleBuilder {
+	var seats []int
 	for i := 0; i <= natives.GetMaxPlayers(); i++ {
 		vehID := natives.GetPlayerVehicleID(i)
-    if vehID != v.id{
-      continue
-    }
-    seat := natives.GetPlayerVehicleSeat(i)
-    if seat <= -1 {
-      continue
-    }
-    seats = append(seats, seat+1)
+		if vehID != v.id {
+			continue
+		}
+		seat := natives.GetPlayerVehicleSeat(i)
+		if seat <= -1 {
+			continue
+		}
+		seats = append(seats, seat+1)
 	}
-  
-  if len(seats) > 4 {
-    v.AttachPlayer(p)
-  }
 
-  for s := 0; s<=4; s++{
-    if seats[s] != 0{
-      continue
-    }
-    natives.PutPlayerInVehicle(p.ID, v.id, s-1)
+	if len(seats) > 4 {
+		v.AttachPlayer(p)
+	}
+
+	for s := 0; s <= 4; s++ {
+		if seats[s] != 0 {
+			continue
+		}
+		natives.PutPlayerInVehicle(p.ID, v.id, s-1)
+		break
+	}
+	return v
+}
+
+func (v *vehicleBuilder) DeleteInRange(player player.PlayerBuilder, distance float64) {
+	requestX, requestY, requestZ := player.GetPos()
+	if distance < 0 {
+		distance = 5.0
+	}
+	for vehicle := 0; vehicle < vehiclesConst.MaxVehicles; vehicle++ {
+		var vehicleX, vehicleY, vehicleZ float32
+		sucess := natives.GetVehiclePos(vehicle, &vehicleX, &vehicleY, &vehicleZ)
+		if !sucess {
+			continue
+		}
+		distanceX := (requestX - vehicleX)
+		distanceY := (requestY - vehicleY)
+		distanceZ := (requestZ - vehicleZ)
+
+		vehicleDistance := math.Sqrt(
+			float64(distanceX)*float64(distanceX) +
+				float64(distanceY)*float64(distanceY) +
+				float64(distanceZ)*float64(distanceZ),
+		)
+		if vehicleDistance > distance {
+			continue
+		}
+
+    natives.DestroyVehicle(vehicle)
     break
-  }
-  return v
+	}
 }
