f3da9136c5581d74472e61e91863ce4f43ee030f
brunoisaac27 <brunoisaac2717@gmail.com>
2023-08-18 23:22:14 -0300

    :sparkles: feat: GetIP
    este commit adiciona o método ao módulo server, que obtem o IP do jogador

--- server/player.go
+++ server/player.go
@@ -1,8 +1,15 @@
 package server
 
 import (
+	"errors"
+
 	"github.com/goseventh/rakstar/chat"
 	"github.com/goseventh/rakstar/internal/natives"
+	"github.com/goseventh/rakstar/player"
+)
+
+var (
+	ErrFailureGetIp = errors.New("Failure to obtain player's IP address.")
 )
 
 func (rb *ServerBuild) Spawn(cb *chat.ChatBuilder) *ServerBuild {
@@ -12,4 +19,14 @@ func (rb *ServerBuild) Spawn(cb *chat.ChatBuilder) *ServerBuild {
 	}
 	natives.SpawnPlayer(rb.playerID)
 	return rb
+}
+
+// Invocar esta função retornará o IP do jogador
+func (rb *ServerBuild) GetIP(player player.PlayerBuilder) (string, error) {
+	var ip string
+	sucess := natives.GetPlayerIp(player.ID, &ip, 16)
+	if !sucess {
+		return "", ErrFailureGetIp
+	}
+  return ip, nil
 }
